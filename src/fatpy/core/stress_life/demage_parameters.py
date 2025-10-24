d8a311f929b880962e9874a8cb4e9e1f4255459c
Ivan Tomac <itomac@fesb.hr>
2025-10-24 20:13:59 +0200

    Code corrected according to reviewer comments.

--- src/fatpy/core/stress_life/demage_parameters.py
+++ src/fatpy/core/stress_life/demage_parameters.py
@@ -1,63 +1,38 @@
 """Damage parameters calculation methods for the stress-life."""
-import inspect
-import warnings
+
 import numpy as np
 from numpy.typing import NDArray
 
-def s_eqa_asme(fat_strength_coef: NDArray[np.float64],
-                 fat_strength_exp: NDArray[np.float64],
-                 yilel_stress: NDArray[np.float64],
-                 stress_amp: NDArray[np.float64],
-                 mean_stress: NDArray[np.float64]
-                 ) -> NDArray[np.uint64]:
-    """
-    Uniaxial high-cycle fatigue criterion using the equivalent stress amplitude
-    based on ASME
 
-    References
-    ----------
-    [1] J. Papuga, I. Vízková, M. Lutovinov, M. Nesládek: Mean stress effect in
+def calc_stress_eq_amp_asme(
+    yield_stress: NDArray[np.float64],
+    stress_amp: NDArray[np.float64],
+    mean_stress: NDArray[np.float64],
+) -> NDArray[np.float64]:
+    r"""Stress-life prediction function based on the ASME mean-stress correction.
+
+    Commputes the value of equivalent stress amplitude, :math:`\sigma_{aeq}` in MPa
+    for given stress values :math:`\sigma_a` and :math:`\sigma_m` representing a
+    single load cycle, using the ASME mean stress correction.
+
+    ### Mathematical formulation:
+
+    $$  \displaystyle\sigma_{aeq}=\frac{\sigma_a}{\left[1-\left(\frac{\sigma_m}{R_e}
+    \right)^2\right]^{1/2} } $$
+
+    Args:
+        yield_stress: :math:`R_e` - Tensile yield strength in [MPa].
+        stress_amp: :math:`\\sigma_a` - Stress amplitude in [MPa].
+        mean_stress: :math:`\\sigma_m` - Mean stress in [MPa].
+
+    Returns:
+        Equivalent stress amplitude by ASME
+
+    References:
+        [1] J. Papuga, I. Vízková, M. Lutovinov, M. Nesládek: Mean stress effect in
         stress-life fatigue prediction re-evaluated, MATEC Web of Conferences 165,
         10018, 2018.
-    
-    Parameters
-    ----------
-    :param fat_strength_coef : NDArray[np.float64] Manson-Coffin and Basquin
-        equation fatigue strength coefficient in [MPa]
-    :param fat_strength_exp : NDArray[np.float64] Manson-Coffin and Basquin
-        equation fatigue strength exponent
-    :param yilel_stress : NDArray[np.float64] Tensile yield strength in [MPa]
-    :param stress_amp : NDArray[np.float64] Stress amplitude in [MPa]
-    :param mean_stress : NDArray[np.float64] Mean stress in [MPa]
-    
-    :return: NDArray[np.uint64] Estimated repetitions N of a given load cycle
-    to failure
     """
-    signature = inspect.signature(s_eqa_asme)
-    for param in signature.parameters.values():
-        if param.name not in locals():
-            raise ValueError(f"Missing argument: {param.name}")
-        if not isinstance(locals()[param.name], np.ndarray):
-            raise TypeError(f"{param.name} must be a numpy ndarray.")
+    stress_aeq = stress_amp / np.sqrt(1 - (mean_stress / yield_stress) ** 2)
 
-    if np.any(fat_strength_coef <= 0):
-        raise ValueError("fat_strength_coef must be positive values.")
-    if np.any(fat_strength_exp >= 0):
-        raise ValueError("fat_strength_exp must be negative values.")
-    if np.any(yilel_stress <= 0):
-        raise ValueError("yilel_stress must be positive values.")
-    if np.any(stress_amp < 0):
-        raise ValueError("stress_amp must be non-negative values.")
-    if np.any(mean_stress/yilel_stress >= 1):
-        raise ValueError("mean_stress/yilel_stress must be less than 1.")
-
-    stress_aeq = stress_amp / np.sqrt(1 - (mean_stress/yilel_stress)**2)
-
-    if np.any(stress_aeq >= fat_strength_coef):
-        warnings.warn("excessive loading detected resulting in N < 1.", UserWarning)
-
-    N = 0.5 * (stress_aeq / fat_strength_coef)**(1/fat_strength_exp)
-
-    return N.astype(np.uint64)
-    
-
+    return stress_aeq
--- tests/stress_life/test_dam_params.py
+++ tests/stress_life/test_dam_params.py
@@ -1,18 +1,13 @@
-"""Damage parameters calculation methods for the stress-life.
-
-"""
+"""Damage parameters calculation methods for the stress-life."""
 
 import numpy as np
-import pytest
-from numpy.typing import NDArray
-from fatpy.core.stress_life.demage_parameters import s_eqa_asme
+from fatpy.core.stress_life.demage_parameters import calc_stress_eq_amp_asme
 
-def test_s_eqa_asme() -> None:
-    fat_strength_coef = np.array([475.4])
-    fat_strength_exp = np.array([-0.078])
-    yilel_stress = np.array([500.0])
-    stress_amp = np.array([180.0])
-    mean_stress = np.array([100.0])
 
-    N = s_eqa_asme(fat_strength_coef, fat_strength_exp, yilel_stress, stress_amp, mean_stress)
-    assert N == np.array([98366])
+def test_calc_stress_eq_amp_asme() -> None:
+    yield_stress = 500.0
+    stress_amp = 180.0
+    mean_stress = 100.0
+
+    sigma_aeq = calc_stress_eq_amp_asme(yield_stress, stress_amp, mean_stress)
+    assert np.around(sigma_aeq, 1) == 183.7
